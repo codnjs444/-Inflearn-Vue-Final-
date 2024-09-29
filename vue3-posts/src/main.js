287fa8fb2f8faf28d589ff0fa705a177eb469384
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-09-29 18:02:46 +0900

    git init

---
+++ vue3-posts/src/main.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css' // 외부 CSS 라이브러리
+import 'bootstrap/dist/js/bootstrap.js' // 외부 JavaScript 라이브러리
+
+import { createApp } from 'vue' // Vue.js 관련 import
+import App from './App.vue' // 메인 Vue 컴포넌트
+
+createApp(App).mount('#app') // 애플리케이션 초기화 및 마운트
