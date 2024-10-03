d2ff0b78646310f0c63c7ea6e26d8b91319ac6e8
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-03 19:46:16 +0900

    study : page + filter 기능 + json server 함수

--- vue3-posts/src/api/posts.js
+++ vue3-posts/src/api/posts.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 // axios
 const posts = [
-  { id: 1, title: '제목1', content: '내용1', createAt: '2021-11-11' },
-  { id: 2, title: '제목2', content: '내용2', createAt: '2022-22-22' },
-  { id: 3, title: '제목3', content: '내용3', createAt: '2023-33-33' },
-  { id: 4, title: '제목4', content: '내용4', createAt: '2024-44-44' },
-  { id: 5, title: '제목5', content: '내용5', createAt: '2025-55-55' }
+  { id: 1, title: '제목1', content: '내용1', createdAt: '2021-11-11' },
+  { id: 2, title: '제목2', content: '내용2', createdAt: '2022-12-22' },
+  { id: 3, title: '제목3', content: '내용3', createdAt: '2023-03-03' },
+  { id: 4, title: '제목4', content: '내용4', createdAt: '2024-04-04' },
+  { id: 5, title: '제목5', content: '내용5', createdAt: '2025-05-05' }
 ]
 
-export function getPosts() {
-  return axios.get('http://localhost:5000/posts')
+export function getPosts(params) {
+  return axios.get('http://localhost:5000/posts', { params })
 }
 
 export function getPostByID(id) {
--- vue3-posts/src/views/posts/PostListView.vue
+++ vue3-posts/src/views/posts/PostListView.vue
@@ -2,6 +2,21 @@
   <div>
     <h2>게시글 목록</h2>
     <hr class="my-4" />
+    <form @submit.prevent="">
+      <div class="row g-3">
+        <div class="col">
+          <input v-model="params.title_like" type="text" class="form-control" />
+        </div>
+        <div class="col">
+          <select v-model="params._limit" class="form-select">
+            <option value="3">3개씩 보기</option>
+            <option value="6">6개씩 보기</option>
+            <option value="9">9개씩 보기</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <hr class="my-4" />
     <div class="row g-3">
       <div v-for="post in posts" :key="post.id" class="col-4">
         <PostItem
@@ -13,7 +28,29 @@
         </PostItem>
       </div>
     </div>
-    <hr class="my-4" />
+    <nav class="mt-5" aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item" :class="{ disabled: !(params._page > 1) }">
+          <a class="page-link" href="#" @click.prevent="params._page--" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li
+          v-for="page in pageCount"
+          :key="page"
+          class="page-item"
+          :class="{ active: params._page === page }"
+        >
+          <a class="page-link" href="#" @click.prevent="params._page = page">{{ page }}</a>
+        </li>
+        <li class="page-item" :class="{ disabled: !(params._page < pageCount) }">
+          <a class="page-link" href="#" @click.prevent="params._page++" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <hr class="my-5" />
     <AppCard>
       <PostDetail :id="1"></PostDetail>
     </AppCard>
@@ -21,7 +58,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 import PostItem from '../../components/posts/PostItem.vue'
 import PostDetail from '@/views/posts/PostDetailView.vue'
 import AppCard from '../../components/AppCard.vue'
@@ -30,28 +67,46 @@ import { getPosts } from '@/api/posts'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-
 const posts = ref([])
+
+const params = ref({
+  _sort: '-createdAt',
+  _page: 1,
+  _limit: 3,
+  title_like: ''
+})
+
+// pagination
+const totalCount = ref(0)
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit))
+
 const fetchPosts = async () => {
   try {
-    const { data } = await getPosts()
+    const { data, headers } = await getPosts(params.value) // async/await 방식
+    console.log('params:', params.value) // 쿼리 파라미터 확인
+
     posts.value = data
+    totalCount.value = headers['x-total-count']
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
-  // 방법 1 [ async 사용 ]
-  // const {data} = await getPosts()
+
+  // 방법 1 [ async/await 사용 ]
+  // const { data } = await getPosts(params.value)
   // posts.value = data
-  // 방법 2 [ promise 사용]
-  // getPosts().then((response) => {
-  //   console.log('response', response)
-  // }) .catch(error => {
+
+  // 방법 2 [ promise 사용 ]
+  // getPosts(params.value).then((response) => {
+  //   posts.value = response.data
+  // }).catch((error) => {
   //   console.log('error:', error)
   // })
-  // 방법 3 [ 바로 주입 ]
-  // {{ dataL posts.value } = await getPosts()}
+
+  // 방법 3 [ 객체 구조 분해 할당 방식으로 바로 주입 ]
+  // ({ data: posts.value } = await getPosts(params.value))
 }
-fetchPosts()
+watchEffect(fetchPosts)
+// fetchPosts()
 
 // http://localhost:5173/posts/1?searchText=hello#world!
 const goPage = (id) => {
