7ff393c4b143673e052e32e459fccf741f41705d
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-05 12:08:10 +0900

    study : createForm 컴포넌트 분리

--- vue3-posts/src/views/posts/PostCreateView.vue
+++ vue3-posts/src/views/posts/PostCreateView.vue
@@ -2,20 +2,14 @@
   <div>
     <h2>게시글 등록</h2>
     <hr class="my-4" />
-    <form @submit.prevent.save>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">제목</label>
-        <input v-model="form.title" type="text" class="form-control" id="title" />
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea v-model="form.content" class="form-control" id="content" rows="3"></textarea>
-      </div>
-      <div class="pt-4">
-        <button class="btn btn-primary me-2" @click="save">저장</button>
-        <button type="button" class="btn btn-outline-dark ma-2" @click="goListPage">목록</button>
-      </div>
-    </form>
+    <PostForm v-model:title="form.title" v-model:content="form.content" @submit.prevent.save>
+      <template #actions>
+        <div class="pt-4">
+          <button class="btn btn-primary me-2" @click="save">저장</button>
+          <button type="button" class="btn btn-outline-dark ma-2" @click="goListPage">목록</button>
+        </div>
+      </template>
+    </PostForm>
   </div>
 </template>
 
@@ -23,6 +17,7 @@
 import { createPost } from '@/api/posts'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import PostForm from '../../components/posts/PostForm.vue'
 
 const router = useRouter()
 const form = ref({
